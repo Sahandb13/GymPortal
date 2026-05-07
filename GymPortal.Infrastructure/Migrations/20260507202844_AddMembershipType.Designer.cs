3edc895c260fe24657519b9d627ee9389a46faeb
Sahand Bagheri <Sahandbagheri@live.se>
2026-05-07 22:42:18 +0200

    hotfix

--- GymPortal.Infrastructure/Migrations/20260507202844_AddMembershipType.Designer.cs
+++ GymPortal.Infrastructure/Migrations/20260507202844_AddMembershipType.Designer.cs
@@ -4,6 +4,7 @@ using GymPortal.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GymPortal.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260507202844_AddMembershipType")]
+    partial class AddMembershipType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GymPortal.Infrastructure/Migrations/20260507202844_AddMembershipType.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymPortal.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMembershipType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Memberships");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Memberships",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Memberships");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Memberships",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
